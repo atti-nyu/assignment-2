2badba311589ceda3f407dcaf1bcfcf1d7e44db7
atti-nyu <am9589@nyu.edu>
2019-12-13 21:49:14 -0500

    requirements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 from subprocess import Popen, PIPE, check_output
 from flask import Flask, redirect, url_for,render_template, request, session
 from flask_wtf.csrf import CSRFProtect
-#from flask_talisman import Talisman
+from flask_talisman import Talisman
 from passlib.hash import sha256_crypt
 
 
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = 'BdAui8H9npasU'
 csrf = CSRFProtect(app)
 
 # talisman use for security 
-#Talisman(app, force_https=False, strict_transport_security=False, session_cookie_secure=False)
+Talisman(app, force_https=False, strict_transport_security=False, session_cookie_secure=False)
 
 #Users file
 Users = {}
@@ -25,18 +25,19 @@ Users = {}
 def index():
     return render_template("index.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['uname'].lower()
 
         if (username in Users.keys()):
-            success = "failure"
+            success = "failed"
+
         else:
             password = sha256_crypt.hash(request.form['pword'])
-            twofa = request.form['2fa']
-            Users[username] = {'password': password, '2fa': twofa}
-            success = "success"
+            twofactor = request.form['2fa']
+            Users[username] = {'password': password, '2fa': twofactor}
+            success = "success! "
             user_file = open("./static/users.txt", "w")
             user_file.write(json.dumps(Users))
             user_file.close()
@@ -45,21 +46,22 @@ def register():
     
     if request.method == 'GET':
         success = "Please register for access."
+        
         return render_template("register.html", success = success)
 
 # spell checker here 
-@app.route('/spell_check', methods=['GET', 'POST'])
+@app.route('/spell_check', methods = ['GET', 'POST'])
 def spell_check():
     if(session.get('logged_in') == True): 
         cpath = os.getcwd()
 
         if request.method == 'POST':
-            outputtext = request.form ['inputtext']
-            textfile = open("./static/text.txt", "w")
+            outputtext = request.form ['inputtext'] 
+            textfile = open("./static/text.txt", "w") 
             textfile.writelines(outputtext)
             textfile.close()
 
-            tmp = subprocess.check_output([cpath + '/static/a.out', cpath + '/static/text.txt', cpath + '/static/wordlist.txt']).decode('utf-8')
+            tmp = subprocess.check_output( [cpath + '/static/a.out', cpath + '/static/text.txt', cpath + '/static/wordlist.txt']).decode('utf-8')
             misspelled = tmp.replace("\n",", ")[:-2]
 
             return render_template("spell_check.html", misspelled = misspelled, outputtext = outputtext)
@@ -69,26 +71,29 @@ def spell_check():
 
     else:
         return redirect(url_for('login'))
-        
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['uname'].lower()
+        username = request.form ['uname'].lower()
 
         if (username in Users.keys()):
             password = request.form['pword']
-            twofa = request.form['2fa']
+            twofactor = request.form['2fa']
 
-            if sha256_crypt.verify(password, Users[username]['password']):
+            if sha256_crypt.verify(password, Users [username] ['password']):
 
-                if (Users[username]['2fa'] == twofa):
+                if (Users [username] ['2fa'] == twofactor):
                     session['logged_in'] = True
                     result = "success"
+
                 else:
-                    result = "Two factor authentication failure"
+                    result = "Two factor authentication failed"
+            
             else:
                 result = "Incorrect password!"
+        
         else:
             result = "Incorrect username!"
         
@@ -96,6 +101,7 @@ def login():
 
     if request.method == 'GET':
         result = "Please login to use this website"
+
         return render_template("login.html", result = result)
 
     
